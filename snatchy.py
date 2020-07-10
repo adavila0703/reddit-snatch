050f9ced639b20a870f31be8a6919dbece56919f
adavila0703 <36370758+adavila0703@users.noreply.github.com>
2020-07-10 17:47:02 -0500

    update

--- snatchy.py
+++ snatchy.py
@@ -1,26 +1,55 @@
-import requests
-import bs4
+from selenium import webdriver
+import time
+from selenium.common.exceptions import InvalidArgumentException, NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 
-request = requests.get('https://www.reddit.com/login/')
-reddit_soup = bs4.BeautifulSoup(request.text, 'lxml')
 
-# username reddit_soup.select('input')[9]['name']
-# password reddit_soup.select('input')[10]['name']
-# token print(reddit_soup.select('input')[2])
+driver = webdriver.Chrome('chromedriver.exe')
 
-session = requests.Session()
+user = ''
+driver.get(f"https://old.reddit.com/login")
+search = driver.find_element_by_id('user_login')
+search.send_keys(user)
 
-# Create the payload
-payload = {'_username':'',
-          '_password':''
-         }
+search = driver.find_element_by_id('passwd_login')
+search.send_keys('')
 
-# Post the payload to the site to log in
-s = session.post("http://www.reddit.com/login/", data=payload)
+search.submit()
+time.sleep(1)
+driver.get(f"https://old.reddit.com/user/{user}/saved/")
+images = driver.find_elements_by_class_name('expando')
+# while True:
+#     stored_pics = []
+#     count = 0
+#     for i in images:
+#         try:
+#             stored_pics.insert(count, i.get_attribute('data-cachedhtml').split()[8].split('"')[1])
+#         except AttributeError:
+#             pass
+#         count += 1
+#
+#     for s in stored_pics:
+#         print(s)
+#         try:
+#             name = s.split()[0].split('/')[3].split('.')[0]
+#         except IndexError:
+#             name = 'null'
+#         try:
+#             driver.get(s)
+#             driver.get_screenshot_as_file(s)
+#             driver.save_screenshot(f'pictures/{name}.png')
+#             time.sleep(.5)
+#         except InvalidArgumentException:
+#             pass
+#     try:
+#         find_next = driver.find_element_by_class_name('next-button')
+#         next_page = find_next.find_element_by_css_selector('a').get_attribute('href')
+#         driver.get(next_page)
+#         print('next page')
+#     except NoSuchElementException:
+#         print('no next page')
+#         break
+driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL't')
 
-# Navigate to the next page and scrape the data
-s = session.get('')
 
-soup = bs4.BeautifulSoup(s.text, 'lxml')
-print(soup)
 
